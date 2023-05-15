77ce70f05982a561b23450bf92740cb842fa4e15
PTB_OFF <94545105+PTB-OFF@users.noreply.github.com>
2023-05-15 12:18:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Azure App Service!');
+  res.end('Node JS app on Azure App Service is working !');
 });
 
 server.listen(port, () => {
